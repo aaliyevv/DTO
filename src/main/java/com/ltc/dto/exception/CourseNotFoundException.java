29223251ae5ca09e22d263c6b7fa28a51d43590b
Ali <aaliyev14291@ada.edu.az>
2026-02-05 10:31:23 +0400

    Initial commit

--- src/main/java/com/ltc/dto/exception/CourseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ltc.dto.exception;
-
-public class CourseNotFoundException extends RuntimeException {
-    public CourseNotFoundException(String message) {
-        super(message);
-    }
-}
