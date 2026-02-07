eba9c4925e35bb04ddc4ca87d3cfdac53daa6207
Ali <aaliyev14291@ada.edu.az>
2026-02-07 17:03:44 +0400

    Service Implementation

--- src/main/java/com/ltc/dto/service/impl/CourseServiceImpl.java
+++ src/main/java/com/ltc/dto/service/impl/CourseServiceImpl.java
@@ -30,5 +30,44 @@ public class CourseServiceImpl implements CourseService {
 
         CourseEntity dbSaving = courseRepo.save(courseEntity);
 
+        CourseResponseDTO courseResponseDTO = new CourseResponseDTO();
+
+        courseResponseDTO.setId(dbSaving.getId());
+        courseResponseDTO.setCourseName(dbSaving.getCourseName());
+        courseResponseDTO.setStudentName(dbSaving.getStudentName());
+        courseResponseDTO.setStudentSurname(dbSaving.getStudentSurname());
+
+        return courseResponseDTO;
+
+
+    }
+
+    @Override
+    public CourseResponseDTO getById(Long id) {
+
+        CourseEntity courseEntity = courseRepo.findById(id).orElseThrow(() ->
+                new CourseNotFoundException("Course Not Found" + id));
+
+        CourseResponseDTO courseResponseDTO = new CourseResponseDTO();
+
+        courseResponseDTO.setId(courseEntity.getId());
+        courseResponseDTO.setCourseName(courseEntity.getCourseName());
+        courseResponseDTO.setStudentName(courseEntity.getStudentName());
+        courseResponseDTO.setStudentSurname(courseEntity.getStudentSurname());
+
+        return courseResponseDTO;
+    }
+
+    @Override
+    public List<CourseResponseDTO> getAll() {
+        return courseRepo.findAll().stream()
+                .map( course ->
+                        new CourseResponseDTO(
+                                course.getId(),
+                                course.getCourseName(),
+                                course.getStudentName(),
+                                course.getStudentSurname())).toList();
+    }
+
 
 }
